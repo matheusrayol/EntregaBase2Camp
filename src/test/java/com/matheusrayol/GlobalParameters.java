df101461c2297ceb1a1d540ac12d4e452c9f4599
Matheus Rayol <matheusrbferreira@gmail.com>
2022-03-29 18:18:44 -0300

    Inclusão da classe GlobalParameters; Inclusão do .gitignore; Inclusão de arquivo dummy de log para uploads; Inclusão do globalParameters.properties para configuração do ambiente.

---
+++ src/test/java/com/matheusrayol/GlobalParameters.java
@@ -0,0 +1,48 @@
+package com.matheusrayol;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GlobalParameters {
+
+    // Declare the global parameters
+    public static String DEFAULT_BROWSER;
+    public static String EXECUTION;
+    public static int DEFAULT_TIMEOUT;
+    public static String DEFAULT_URL;
+    public static String DEFAULT_DOWNLOAD_PATH;
+    public static String DEFAULT_SCREENSHOT_PATH;
+    public static String DEFAULT_USERNAME;
+    public static String DEFAULT_PASSWORD;
+
+    private final Properties properties;
+
+    public GlobalParameters() {
+        // Load the properties file
+        properties = new Properties();
+        InputStream input;
+
+        try {
+            input = new FileInputStream("src/test/globalParameters.properties");
+            properties.load(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Set the global parameters
+        DEFAULT_BROWSER = properties.getProperty("DEFAULT_BROWSER");
+        EXECUTION = properties.getProperty("EXECUTION");
+        DEFAULT_TIMEOUT = Integer.parseInt(properties.getProperty("DEFAULT_TIMEOUT"));
+        DEFAULT_URL = properties.getProperty("DEFAULT_URL");
+        DEFAULT_DOWNLOAD_PATH = properties.getProperty("DEFAULT_DOWNLOAD_PATH");
+        DEFAULT_SCREENSHOT_PATH = properties.getProperty("DEFAULT_SCREENSHOT_PATH");
+        DEFAULT_USERNAME = properties.getProperty("DEFAULT_USERNAME");
+        DEFAULT_PASSWORD = properties.getProperty("DEFAULT_PASSWORD");
+
+    }
+}
