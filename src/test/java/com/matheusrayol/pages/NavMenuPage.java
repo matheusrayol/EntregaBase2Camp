6a854d22e059973e288d6ccaf304793910596b00
Matheus Rayol <matheusrbferreira@gmail.com>
2022-04-04 18:43:06 -0300

    Remoção de elementos sem uso no NavMenuPage.java

--- src/test/java/com/matheusrayol/pages/NavMenuPage.java
+++ src/test/java/com/matheusrayol/pages/NavMenuPage.java
@@ -11,55 +11,20 @@ public class NavMenuPage extends PageBase {
     // Mapping - Menu
     // Optei por mapear os elementos pelo cssSelector, tendo em vista que o Mantis é um sistema multilinguagem e caso
     // o usuário altere o idioma, os elementos não ficariam corretamente mapeados com o mapeamento por name.
-    By myviewLink = By.cssSelector("a[href='/my_view_page.php']");
     By viewIssuesLink = By.cssSelector("a[href='/view_all_bug_page.php']");
     By reportIssueLink = By.cssSelector("a[href='/bug_report_page.php']");
-    By changelogLink = By.cssSelector("a[href='/changelog_page.php']");
-    By roadmapLink = By.cssSelector("a[href='/roadmap_page.php']");
-    By summaryLink = By.cssSelector("a[href='/summary_page.php']");
-    By manageLink = By.cssSelector("a[href='/manage_overview_page.php']");
-    By myAccountLink = By.cssSelector("a[href='/account_page.php']");
-    By logoutLink = By.cssSelector("a[href='/logout_page.php']");
 
     // Mapping - Project Selection
     By projectSelectionComboBox = By.cssSelector("td[class='login-info-right'] select");
 
 
     // Actions
-    public void clicarNoLinkMyViews() {
-        click(myviewLink);
-    }
-
     public void clicarNoLinkViewIssues() {
         click(viewIssuesLink);
     }
 
     public void clicarNoLinkReportIssue() {
         click(reportIssueLink);
-    }
-
-    public void clicarNoLinkChangelog() {
-        click(changelogLink);
-    }
-
-    public void clicarNoLinkRoadmap() {
-        click(roadmapLink);
-    }
-
-    public void clicarNoLinkSummary() {
-        click(summaryLink);
-    }
-
-    public void clicarNoLinkManage() {
-        click(manageLink);
-    }
-
-    public void clicarNoLinkMyAccount() {
-        click(myAccountLink);
-    }
-
-    public void clicarNoLinkLogout() {
-        click(logoutLink);
     }
 
     public void selecionarProjeto(String projectName) {
